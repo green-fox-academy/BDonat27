b8a5be76e52955c5c9f794cf963eaf2b17e689cf
Joe <donat.bagi08@gmail.com>
2021-10-25 14:32:13 +0200

    reaper task

--- week-02/day4-5/Tömbök/src/Tasks/Append.java
+++ week-02/day4-5/Tömbök/src/Tasks/Append.java
@@ -1,11 +1,15 @@
+package Tasks;
+
 import java.util.Arrays;
 
 public class Append {
     public static void main(String[] args) {
         String[] animals = {"koal", "pand", "zebr"};
-        animals[0] = "koala";
-        animals[1] = "panda";
-        animals[2] = "zebra";
-        System.out.println(Arrays.toString(animals));
+        for (int i = 0; i < animals.length ; i++) {
+
+            System.out.println(animals[i] + "a");
+        }
+
+
     }
 }
--- week-02/day4-5/Tömbök/src/Tasks/Matrix.java
+++ week-02/day4-5/Tömbök/src/Tasks/Matrix.java
@@ -1,22 +1,36 @@
 package Task;
 
+import java.util.Arrays;
+
 public class Matrix {
     public static void main(String[] args) {
-        int[][]twoDimentonal = {{1 ,0 ,0 ,0}, {0 ,1 ,0 ,0}, {0 ,0 ,1 ,0}, {0, 0 ,0 ,1}};
+        int Matrix[][] = new int[4][4];
         for (int i = 0; i < 4; i++) {
-            for (int j = 0; j <4 ; j++) {
+            for (int j = 0; j < 4; j++) {
 
-                System.out.print(twoDimentonal[i][j]+ " ");
+                if (i == j) {
+                    Matrix[i][j] = 1;
+                } else {
+                    Matrix[i][j] = 0;
+                }
+
+                System.out.print(Matrix[i][j] + " ");
+
+
+
             }
-            System.out.println();
+
+            System.out.println("");
         }
+
+
     }
 
 
-
-
-
-
-
-
 }
+
+
+
+
+
+
